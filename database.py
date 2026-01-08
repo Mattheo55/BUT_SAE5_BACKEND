7f36408296a2a01e3ed4efa7292f3e5782b86a85
Ubuntu <ubuntu@vps-dc5fd3ad.vps.ovh.net>
2026-01-08 10:02:35 +0000

    Mise en place complete sur vps

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3306/sae5"
+load_dotenv()
+
+SQALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
